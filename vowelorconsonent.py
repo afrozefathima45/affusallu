01fc923243c1b8a237f28e42234d5449f0ae6b2e
afrozefathima45 <42863100+afrozefathima45@users.noreply.github.com>
2019-02-07 14:46:50 +0530

    Update vowelorconsonent.py

--- vowelorconsonent.py
+++ vowelorconsonent.py
@@ -1,7 +1,7 @@
-char=input("enter the character to check consonent or vowels:")
-if(char=='a' or char=='e' or char=='i' or char=='o' or char=='u'):
-  print("vowels")
-elif(char=='b' or char=='c' or char=='d' or char=='f' or char=='g' or char=='h' or char=='j' or char=='k' or char=='l' or char=='m' or char=='n' or char=='p' or char=='q' or char=='r' or char=='s' or char=='t' or char=='v' or char=='w' or char=='x' or char=='y' or char=='z'):
-  print("consonent")
+char=input()
+if(char=='a' or char=='e' or char=='i' or char=='o' or char=='u' or char=='A' or char=='E' or char=='I' or char=='O' or char=='U'):
+  print("Vowel")
+elif(char=='b' or char=='c' or char=='d' or char=='f' or char=='g' or char=='h' or char=='j' or char=='k' or char=='l' or char=='m' or char=='n' or char=='p' or char=='q' or char=='r' or char=='s' or char=='t' or char=='v' or char=='w' or char=='x' or char=='y' or char=='z' or char=='A' or char=='B' or char=='C' or char=='D' or char=='E' or char=='F' or char=='G' or char=='H' or char=='I' or char=='J' or char=='K' or char=='L' or char=='M' or char=='N' or char=='O' or char=='P' or char=='Q' or char=='R' or char=='S' or char=='T' or char=='U' or char=='V' or char=='W' or char=='X' or char=='Y' or char=='Z'  ):
+  print("Consonent")
 else:
   print("invalid")
