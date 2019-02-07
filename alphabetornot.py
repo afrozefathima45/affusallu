592207032a92a1c418fd827b5d952ca4ea2c4f36
afrozefathima45 <42863100+afrozefathima45@users.noreply.github.com>
2019-02-07 14:24:29 +0530

    Update alphabetornot.py

--- alphabetornot.py
+++ alphabetornot.py
@@ -1,9 +1,8 @@
-print("Enter '0' for exit.");
-num = input("Enter any character or alphabet: ");
+num = input()
 if num == '0':
-    exit();
+    exit()
 else:
     if((num>='a' and num<='z') or (num>='A' and num<='Z')):
-    	print(num, "is an alphabet.");
+    	print( "Alphabet")
     else:
-    	print(num, "is not an alphabet.");
+    	print("No")
