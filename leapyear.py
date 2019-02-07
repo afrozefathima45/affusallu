209b5ec563c1f8e8c90a14f8a22828de45fd63c2
afrozefathima45 <42863100+afrozefathima45@users.noreply.github.com>
2019-02-07 13:46:59 +0530

    Create leapyear.py

---
+++ leapyear.py
@@ -0,0 +1,11 @@
+a = 2000
+if (a % 4) == 0:
+   if (a % 100) == 0:
+       if (a % 400) == 0:
+           print("{0} is a leap year".format(a))
+       else:
+           print("{0} is not a leap year".format(a))
+   else:
+       print("{0} is a leap year".format(a))
+else:
+   print("{0} is not a leap year".format(a))
